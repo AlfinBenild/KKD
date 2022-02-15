3074357a4b0a67d74843d4ec416f7cdea774e615
Alfin Benild <alfinbenild03@gmail.com>
2022-02-15 20:54:47 +0530

    Added basic version of voice command recognizer

---
+++ voice.py
@@ -0,0 +1,164 @@
+import os
+import numpy as np
+import tensorflow as tf
+import pyaudio
+import wave 
+from pydub import AudioSegment
+from pydub.playback import play
+from scipy.io import wavfile
+from scipy.fftpack import fft
+from scipy.signal import get_window
+import matplotlib.pyplot as plt
+import tflite_runtime.interpreter as tflite
+
+
+
+def record(command):
+    # Record in chunks of 1024 samples
+    chunk = 1024
+    # 16 bits per sample
+    sample_format = pyaudio.paInt16
+    channels = 1
+    sample_rate = 16000
+    seconds = 1.5
+    filename = "{}.wav".format(command)
+
+    # Create an interface to PortAudio
+    pa = pyaudio.PyAudio()
+    stream = pa.open(format = sample_format, channels = channels,
+                    rate = sample_rate, input = True, frames_per_buffer = chunk)
+    print("Recording...")
+    # Initialize array that be used for storing frames
+    frames = [] 
+
+    for i in range(0, int(sample_rate / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    stream.stop_stream()
+    stream.close()
+    # Terminate - PortAudio interface
+    pa.terminate()
+
+    print("Done !!!")
+
+    # Save the recorded data in a .wav format
+    sf = wave.open(filename, 'wb')
+    sf.setnchannels(channels)
+    sf.setsampwidth(pa.get_sample_size(sample_format))
+    sf.setframerate(sample_rate)
+    sf.writeframes(b''.join(frames))
+    sf.close()
+    return filename
+
+def decode_audio(audio_binary):
+    audio, _ = tf.audio.decode_wav(contents = audio_binary)
+    return tf.squeeze(audio, axis = -1)
+
+def normalize_audio(audio):
+    audio = audio/np.max(np.abs(audio))
+    return audio
+
+def frame_audio(audio, FFT_size=2048, hop_size=10, sample_rate=16000):
+    # hop_size in ms
+    
+    audio = np.pad(audio, int(FFT_size / 2), mode='reflect')
+    frame_len = np.round(sample_rate * hop_size / 1000).astype(int)
+    frame_num = int((len(audio) - FFT_size) / frame_len) + 1
+    frames = np.zeros((frame_num,FFT_size))
+    
+    for n in range(frame_num):
+        frames[n] = audio[n*frame_len:n*frame_len+FFT_size]
+    
+    return frames
+
+def freq_to_mel(freq):
+    return 2595.0 * np.log10(1.0 + freq / 700.0)
+
+def met_to_freq(mels):
+    return 700.0 * (10.0**(mels / 2595.0) - 1.0)
+
+def get_filter_points(fmin, fmax, mel_filter_num, FFT_size, sample_rate=16000):
+    fmin_mel = freq_to_mel(fmin)
+    fmax_mel = freq_to_mel(fmax)
+    mels = np.linspace(fmin_mel, fmax_mel, num=mel_filter_num+2)
+    freqs = met_to_freq(mels)
+    
+    return np.floor((FFT_size + 1) / sample_rate * freqs).astype(int), freqs
+
+def get_filters(filter_points, FFT_size):
+    filters = np.zeros((len(filter_points)-2,int(FFT_size/2+1)))
+    
+    for n in range(len(filter_points)-2):
+        filters[n, filter_points[n] : filter_points[n + 1]] = np.linspace(0, 1, filter_points[n + 1] - filter_points[n])
+        filters[n, filter_points[n + 1] : filter_points[n + 2]] = np.linspace(1, 0, filter_points[n + 2] - filter_points[n + 1])
+    
+    return filters
+
+def dct(dct_filter_num, filter_len):
+    basis = np.empty((dct_filter_num,filter_len))
+    basis[0, :] = 1.0 / np.sqrt(filter_len)
+    
+    samples = np.arange(1, 2 * filter_len, 2) * np.pi / (2.0 * filter_len)
+
+    for i in range(1, dct_filter_num):
+        basis[i, :] = np.cos(i * samples) * np.sqrt(2.0 / filter_len)
+        
+    return basis
+
+
+def preprocess(filepath):
+    sample_rate, audio = wavfile.read(filepath)
+    audio = normalize_audio(audio)
+
+    hop_size = 15 #ms
+    FFT_size = 2048
+
+    audio_framed = frame_audio(audio, FFT_size=FFT_size, hop_size=hop_size, sample_rate=sample_rate)
+    window = get_window("hann", FFT_size, fftbins=True)
+    audio_win = audio_framed * window
+    audio_winT = np.transpose(audio_win)
+
+    audio_fft = np.empty((int(1 + FFT_size // 2), audio_winT.shape[1]), dtype=np.complex64, order='F')
+
+    for n in range(audio_fft.shape[1]):
+        audio_fft[:, n] = fft(audio_winT[:, n], axis=0)[:audio_fft.shape[0]]
+
+    audio_fft = np.transpose(audio_fft)
+    audio_power = np.square(np.abs(audio_fft))
+    freq_min = 0
+    freq_high = sample_rate / 2
+    mel_filter_num = 10
+    filter_points, mel_freqs = get_filter_points(freq_min, freq_high, mel_filter_num, FFT_size, sample_rate=16000)
+    filters = get_filters(filter_points, FFT_size)
+    enorm = 2.0 / (mel_freqs[2:mel_filter_num+2] - mel_freqs[:mel_filter_num])
+    filters *= enorm[:, np.newaxis]
+    audio_filtered = np.dot(filters, np.transpose(audio_power))
+    audio_log = 10.0 * np.log10(audio_filtered)
+    dct_filter_num = 40
+
+    dct_filters = dct(dct_filter_num, mel_filter_num)
+    cepstral_coefficents = np.dot(dct_filters, audio_log)
+    return cepstral_coefficents
+
+
+commands_to_ids = {'Down' : 0, 'Engine' : 1, 'Off' : 2, 'On' : 3, 'One' : 4, 'Three' : 5, 'Two' : 6, 'Up' : 7, 'Window' : 8, 'Wiper' : 9}
+ids_to_commands = {0 : 'Down', 1: 'Engine', 2 : 'Off', 3 : 'On', 4 : 'One', 5 : 'Three', 6 : 'Two',  7 : 'Up', 8 : 'Window', 9 : 'Wiper'}
+
+interpreter = tflite.Interpreter("voice_model.tflite")
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+input_shape = input_details[0]['shape']
+input("Press Enter to Continue !!! \n")
+command = record('voice1')
+input_data = preprocess(command).reshape(1, 40, 99, 1)
+interpreter.set_tensor(input_details[0]['index'], input_data)
+interpreter.invoke()
+output_data = interpreter.get_tensor(output_details[0]['index'])
+final_output = ids_to_commands[np.argmax(output_data, axis = 1)[0]]
+
+print(final_output)
+
